674304e7701a3cf0d30ad58c5d684f91defa8d2c
Solee Lee <carolinesoleelee@gmail.com>
2018-09-26 19:27:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  new = []
+  names.each do |x|
+  new << x  
+end
+new    
 end
 
 def summon_captain_planet# code an argument here
