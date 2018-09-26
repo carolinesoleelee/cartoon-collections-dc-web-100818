a3b1ae24b03d9666cd813da66f530571a1776e7e
Solee Lee <carolinesoleelee@gmail.com>
2018-09-26 19:35:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,7 @@
 def roll_call_dwarves(names)
-  new = []
   names.each_with_index do |x,index|
-  new << puts = "#{index}: #{value}"
+ puts = "#{index}: #{value}"
 end
-new
 end
 
 def summon_captain_planet# code an argument here
